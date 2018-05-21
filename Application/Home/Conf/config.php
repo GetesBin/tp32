d57a3c76644713f1469d590843031bc90a0d9f8f
GetesBin <34531742+GetesBin@users.noreply.github.com>
2018-05-21 13:03:39 +0800

    Update config.php

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -2,10 +2,10 @@
 return array(
 	/* 数据库设置 */
 	'DB_TYPE'               =>  'mysql',     // 数据库类型
-	'DB_HOST'               =>  'localhost', // 服务器地址
-	'DB_NAME'               =>  'tp323',          // 数据库名
-	'DB_USER'               =>  'root',      // 用户名
-	'DB_PWD'                =>  'root',          // 密码
+	'DB_HOST'               =>  'qdm114438419.my3w.com', // 服务器地址
+	'DB_NAME'               =>  'qdm114438419_db',          // 数据库名
+	'DB_USER'               =>  'qdm114438419',      // 用户名
+	'DB_PWD'                =>  'root123456',          // 密码
 	'DB_PORT'               =>  '3306',        // 端口
 	'DB_PREFIX'             =>  'tp_',    // 数据库表前缀
 	'DB_PARAMS'          =>  array(), // 数据库连接参数
