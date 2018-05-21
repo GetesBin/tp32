a67b4168979755277a6a9abbe34d91970af11531
GatesBin <gatesbin@163.com>
2018-05-21 13:10:14 +0800

    清除缓存

--- Application/Runtime/Data/_fields/tp323.tp_store.php
+++
@@ -1,1 +0,0 @@
-a:6:{i:0;s:2:"ID";i:1;s:5:"goods";i:2;s:5:"price";i:3;s:3:"uid";s:3:"_pk";s:2:"ID";s:5:"_type";a:4:{s:2:"ID";s:7:"int(10)";s:5:"goods";s:11:"varchar(50)";s:5:"price";s:7:"int(10)";s:3:"uid";s:7:"int(10)";}}
--- Application/Runtime/Data/_fields/tp323.tp_tasklist.php
+++
@@ -1,1 +0,0 @@
-a:5:{i:0;s:2:"ID";i:1;s:8:"taskname";i:2;s:5:"money";s:3:"_pk";s:2:"ID";s:5:"_type";a:3:{s:2:"ID";s:7:"int(10)";s:8:"taskname";s:11:"varchar(50)";s:5:"money";s:11:"varchar(50)";}}
